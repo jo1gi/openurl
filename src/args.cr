2551e355e0074ca71cbcacd7b892ca617652d40e
Joakim Holm <joakimholm@protonmail.com>
2021-05-08 13:46:39 +0200

    Added support for loading a file of links

--- src/args.cr
+++ src/args.cr
@@ -3,21 +3,27 @@ require "option_parser"
 class Args
   getter config : String | Nil
   getter output = false
-  getter url : String = ""
+  getter urls = [] of String
 
   def initialize
     OptionParser.parse do |parser|
       parser.banner = "Usage: openurl [options] URL"
       parser.on("-c CONFIG", "--config CONFIG", "Path to alternative config file") { |c| @config = c }
       parser.on("-s", "--show-output", "Display the output of the command") { @output = true }
+      parser.on("-f FILE", "--file FILE", "Open all links in a file") { |f|
+	if !File.exists?(f)
+	  abort "File '#{f}' does not exist"
+	end
+	@urls = @urls.concat(File.read_lines(f))
+      }
       parser.on("-h", "--help", "Show help menu") do
         puts parser
         exit
       end
       parser.unknown_args() do |args|
         if args.size != 0
-          @url = args[0]
-        else
+	  @urls = @urls.concat(args)
+	elsif @urls.size == 0
           abort "An url is required"
         end
       end
--- src/openurl.cr
+++ src/openurl.cr
@@ -3,16 +3,23 @@ require "./config"
 require "./command"
 require "./rules"
 
+def open_link(url : String, config, opts)
+  # Finding command
+  result = Rules.find_command(url, config)
+  if result.nil?
+    abort "Could not find matching program"
+  end
+  # Running command
+  command, args = Command.parse_command(result, url)
+  Process.run(command, args: args, output: opts.output ? STDOUT : Process::Redirect::Close, input: STDIN, shell: true)
+end
+
 # Loading command line arguments
 opts = Args.new
 # Loading config file
 config_path = opts.config || Config.find_config
 config = Config.load_config(config_path)
-# Finding command
-result = Rules.find_command(opts.url, config)
-if result.nil?
-  abort "Could not find matching program"
+# Opening links
+opts.urls.each do |url|
+  open_link(url, config, opts)
 end
-# Running command
-command, args = Command.parse_command(result, opts.url)
-Process.run(command, args: args, output: opts.output ? STDOUT : Process::Redirect::Close, input: STDIN, shell: true)
