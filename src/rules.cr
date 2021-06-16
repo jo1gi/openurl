ba629415faee9713ef49a44939f8116ca81f222a
Joakim Holm <joakimholm@protonmail.com>
2021-06-16 12:58:34 +0200

    Added new rules and support for rules defined in functions

--- src/rules.cr
+++ src/rules.cr
@@ -3,14 +3,46 @@ module Rules
 
   # Stores all of the available rules for urls to be tested against
   private RULES = {
-    "protocol"  => /^[^:]+/,
-    "domain"    => /(?<=:\/\/)[^\/]+/,
-    "last_file" => /[^\/]+$/,
-    "filetype"  => /(?<=\.)[^\.\/]+$/,
-    "url"       => /.+/,
+    "protocol"  => /^([^:]+)/,
+    "domain"    => /(?<=:\/\/)([^\/]+)/,
+    "last_file" => /([^\/]+)(?:\?.*)?$/,
+    "filetype"  => /(?<=\.)([^\/\.\?]+)(?:\?.*)?$/,
+    "url"       => /(.+)/,
+    "path"      => /(?::\/\/[^\/]+)([^\?]+)/,
+    "params"    => ->test_params(String, YAML::Any),
   }
 
-  # Tests a url on a specific regex parameter from `tests`
+  # Returns a Hash with each url parameter
+  private def find_params(url : String) : Hash(String, String)
+    params = {} of String => String
+    matches = url.match(/(?:[\?&])([^=]+=[^=&]+)/)
+    if matches
+      matches.captures.each do |param|
+        split = param.not_nil!.split("=")
+        params[split[0]] = split[1]
+      end
+    end
+    return params
+  end
+
+  # Tests if the params in the url matches the params in the config
+  private def test_params(url : String, options : YAML::Any) : Bool
+    if options.as_h?
+      params = find_params(url)
+      options.as_h.each do |key, value|
+        if params[key]? && value.as_s? && !Regex.new(value.as_s).match(params[key]).nil?
+          next
+        else
+          return false
+        end
+      end
+      return true
+    else
+      return false
+    end
+  end
+
+  # Tests a url on a regex from `RULES`
   private def make_regex_test(url : String, test : Regex, value : String) : Bool
     matches = url.match(test)
     if matches && matches.size >= 1
@@ -27,8 +59,16 @@ module Rules
     end
     RULES.each do |name, test|
       if option[name]?
-        if make_regex_test(url, test, option[name].as_s)
-          next
+        # Running regex test
+        if test.is_a?(Regex)
+          if make_regex_test(url, test, option[name].as_s)
+            next
+          end
+          # TODO Running function test
+        elsif test.is_a?(Proc(String, YAML::Any, Bool))
+          if test.call(url, option[name])
+            next
+          end
         end
         return false
       end
